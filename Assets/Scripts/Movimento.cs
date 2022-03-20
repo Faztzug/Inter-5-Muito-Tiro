6362e8076a0b62263b04496404c3713c730334a5
Faztz Nya <faztzug@gmail.com>
2022-03-20 19:06:47 -0300

    fix de movimentação

---
+++ Assets/Scripts/Movimento.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    [Header("Character Values")]
+    [SerializeField]  private float speed = 5f;
+    private CharacterController controller;
+    private Camera cam;
+
+    [Header("Gravity Values")]
+    [SerializeField] private float gravity = 1f;
+    private float gravityAcceleration;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        Vector3 vertical = Input.GetAxis("Vertical") * cam.transform.forward;
+        Vector3 horizontal = Input.GetAxis("Horizontal") * cam.transform.right;
+
+        // if (vertical.magnitude > 0.1 || horizontal.magnitude > 0.1)
+        // {
+        //     Vector3 direcaoCamera = Camera.main.transform.forward;
+        //     transform.forward = direcaoCamera;
+        // }
+        if(controller.isGrounded)
+        {
+            gravityAcceleration = 0f;
+        }
+        else
+        {
+            gravityAcceleration -= gravity * Time.deltaTime;
+        }
+
+        Vector3 movement = (vertical + horizontal) * Time.deltaTime;
+        movement.y = gravityAcceleration * Time.deltaTime;
+
+
+        controller.Move(movement * speed);
+
+        // controller.SimpleMove(vertical * speed);
+        // controller.SimpleMove(horizontal * speed);
+    }
+}
