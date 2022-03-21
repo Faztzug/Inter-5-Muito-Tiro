3b7a4a18cad4c5219d9d766929dffdeb32cdc98b
Faztz Nya <faztzug@gmail.com>
2022-03-21 00:26:59 -0300

    Started IK animation

--- Assets/Scripts/Movimento.cs
+++ Assets/Scripts/Movimento.cs
@@ -7,8 +7,10 @@ public class Movimento : MonoBehaviour
     [Header("Character Values")]
     [SerializeField]  private float speed = 5f;
     [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private Transform lookAtObj;
     private CharacterController controller;
     private Camera cam;
+    private Animator anim;
 
     [Header("Gravity Values")]
     [SerializeField] private float gravity = 1f;
@@ -18,42 +20,63 @@ public class Movimento : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         cam = Camera.main;
+        anim = GetComponent<Animator>();
+        
+        if(lookAtObj == null && cam.transform.GetChild(0) != null) lookAtObj = cam.transform.GetChild(0);
     }
 
     private void Update()
     {
         Movement();
+        Animations();
     }
 
     private void Movement()
     {
-        Vector3 vertical = Input.GetAxis("Vertical") * cam.transform.forward;
+        MoveRotation();
+
+        MoveInput();
+    }
+
+    private void Animations()
+    {
+        
+    }
+
+    void OnAnimatorIK()
+    {
+        anim.SetLookAtPosition(lookAtObj.position);
+        anim.SetLookAtWeight(1);
+    }
+
+    private void MoveRotation()
+    {
+        var camRotation = cam.transform.rotation;
+        var objRotation = transform.rotation;
+        Vector3 setRotation = new Vector3(objRotation.eulerAngles.x, camRotation.eulerAngles.y, objRotation.eulerAngles.z);
+        transform.eulerAngles = setRotation;
+        Debug.Log(camRotation.y);
+    }
+
+    private void MoveInput()
+    {
+        Vector3 vertical = Input.GetAxis("Vertical") * transform.forward;
         Vector3 horizontal = Input.GetAxis("Horizontal") * cam.transform.right;
 
-        // if (vertical.magnitude > 0.1 || horizontal.magnitude > 0.1)
-        // {
-        //     Vector3 direcaoCamera = Camera.main.transform.forward;
-        //     transform.forward = direcaoCamera;
-        // }
         if(controller.isGrounded)
         {
+            gravityAcceleration = 0f;
             if(Input.GetButtonDown("Jump")) gravityAcceleration = jumpForce;
-            else gravityAcceleration = 0f;
+            else gravityAcceleration = -gravity * 10f * Time.deltaTime;
         }
         else
         {
             gravityAcceleration -= gravity * Time.deltaTime;
         }
 
-        
-
         Vector3 movement = (vertical + horizontal) * Time.deltaTime;
         movement.y = gravityAcceleration * Time.deltaTime;
 
-
         controller.Move(movement * speed);
-
-        // controller.SimpleMove(vertical * speed);
-        // controller.SimpleMove(horizontal * speed);
     }
 }
