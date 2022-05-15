72f2bc495d2508727d7df93ea19d9e3a0a95776e
Faztz Nya <faztzug@gmail.com>
2022-05-15 19:56:11 -0300

    Introdução Skip e primeira cena que carrega

---
+++ Assets/CutsceneSkip.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneSkip : MonoBehaviour
+{    
+    [SerializeField] private string sceneName;
+    [SerializeField] private float timeToAutoSkip = 60f;
+    public float cronomether = 0;
+    void Start()
+    {
+        StartCoroutine(AutoLoad());
+    }
+    void Update()
+    {
+        cronomether += 1 * Time.unscaledDeltaTime;
+    }
+    IEnumerator AutoLoad()
+    {
+        yield return new WaitForSecondsRealtime(timeToAutoSkip);
+
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
