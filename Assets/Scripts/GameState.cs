e9c7a1da3d2d58919e9431283c8d950214d5541d
Faztz Nya <faztzug@gmail.com>
2022-05-26 03:17:42 -0300

    Mouse Sensibility possibility

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public enum SpeedState
 {
@@ -11,6 +12,27 @@ public enum SpeedState
 
 public class GameState : MonoBehaviour
 {
+    public const float kDefaultMouseSpeedY = 2;
+    public const float kDefaultMouseSpeedX = 200;
+    public const float kDefaultMouseAccelarationY = 0.1f;
+    public const float kDefaultMouseAccelarationX = 0.2f;
+
+    [Header("Save Data")]
+    [Range(0.1f, 2f)] [SerializeField] private float mouseSensibility;
+    public float MouseSensibility 
+    { 
+        get => mouseSensibility; 
+        set => mouseSensibility = value;
+    }
+    [Range(0.1f, 5f)] [SerializeField] private float mouseAccelaration;
+    public float MouseAccelaration 
+    { 
+        get => mouseAccelaration; 
+        set => mouseAccelaration = value;
+    }
+
+    [Header("Game Data")]
+    [SerializeField] private CinemachineFreeLook freeLook;
     public Transform bodyPartHead;
     public Transform bodyPartChest;
     public Transform bodyPartPelvis;
@@ -43,6 +65,14 @@ public class GameState : MonoBehaviour
         var rng = Random.Range(0, bodyPartsList.Count);
         return bodyPartsList[rng];
     }
+    private void SetMouseSpeed()
+    {
+        freeLook.m_YAxis.m_MaxSpeed = kDefaultMouseSpeedY * mouseSensibility;
+        freeLook.m_XAxis.m_MaxSpeed = kDefaultMouseSpeedX * mouseSensibility;
+        
+        freeLook.m_YAxis.m_AccelTime = kDefaultMouseAccelarationY * mouseAccelaration;
+        freeLook.m_XAxis.m_AccelTime = kDefaultMouseAccelarationX * mouseAccelaration;
+    }
 
     void Update()
     {
@@ -54,6 +84,7 @@ public class GameState : MonoBehaviour
                 Debug.LogWarning("GOD MODE: " + GodMode);
             }
         }
+        SetMouseSpeed();
     }
 
 
